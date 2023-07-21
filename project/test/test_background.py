a4df6fa2430d8fa8ea1b935a888a393b80e9dede
ettynan <ettynan@gmail.com>
2023-07-20 22:53:37 -0700

    Adding testing for background model

--- project/test/test_background.py
+++ project/test/test_background.py
@@ -2,22 +2,23 @@ import unittest
 from unittest.mock import patch
 from project.background.model import Background
 
+
 STAR_COUNT = 10
 STAR_COLOR_HIGH = 8
 STAR_COLOR_LOW = 4
 
 class TestBackground(unittest.TestCase):
 
-    @patch('background.model.random')
-    @patch('background.model.pyxel')
+    @patch('project.background.model.random')
+    @patch('project.background.model.pyxel')
     def setUp(self, mock_pyxel, mock_random):
         mock_pyxel.width = 100
         mock_pyxel.height = 100
         mock_random.return_value = 0.5
         self.background = Background()
 
-    @patch('background.model.random')
-    @patch('background.model.pyxel')
+    @patch('project.background.model.random')
+    @patch('project.background.model.pyxel')
     def test_init(self, mock_pyxel, mock_random):
         mock_pyxel.width = 100
         mock_pyxel.height = 100
@@ -31,7 +32,7 @@ class TestBackground(unittest.TestCase):
             self.assertEqual(y, 0.5 * mock_pyxel.height)
             self.assertEqual(speed, 0.5 * 1.5 + 1)
 
-    @patch('background.model.pyxel')
+    @patch('project.background.model.pyxel')
     def test_update(self, mock_pyxel):
         mock_pyxel.height = 100
 
@@ -40,7 +41,7 @@ class TestBackground(unittest.TestCase):
             x, y, speed = star
             self.assertLess(y, mock_pyxel.height)
 
-    @patch('background.model.pyxel')
+    @patch('project.background.model.pyxel')
     def test_draw(self, mock_pyxel):
         self.background.draw()
 
