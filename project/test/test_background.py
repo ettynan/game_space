2d16efa9a20e884b806aa5a3b91467f215273c2e
ettynan <ettynan@gmail.com>
2023-07-21 20:31:09 -0700

    Adding test for background module

--- project/test/test_background.py
+++ project/test/test_background.py
@@ -1,7 +1,8 @@
+'''Module to test the project.background.model module'''
+
 import unittest
 from unittest.mock import patch
 from project.background.model import Background
-
 
 STAR_COUNT = 10
 STAR_COLOR_HIGH = 8
