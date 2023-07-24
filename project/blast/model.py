c744c54d5fb2a4779919cf120bd3247044d345f8
ettynan <ettynan@gmail.com>
2023-07-23 23:00:37 -0700

    Adding test_blast.py, minor changes to blast

--- project/blast/model.py
+++ project/blast/model.py
@@ -6,12 +6,12 @@ from data.logger import get_logger
 _log = get_logger(__name__)
 BLAST_START_RADIUS = 1
 BLAST_END_RADIUS = 8
-BLAST_COLOR_IN = 7
-BLAST_COLOR_OUT = 10
 
 class Blast:
     '''Destructive blast'''
     blast_list = []
+    BLAST_COLOR_IN = 7
+    BLAST_COLOR_OUT = 10
 
     def __init__(self, x, y):
         self.x = x
@@ -19,6 +19,8 @@ class Blast:
         self.radius = BLAST_START_RADIUS
         self.alive = True
         self.blast_list.append(self)
+        self.BLAST_COLOR_IN = 7
+        self.BLAST_COLOR_OUT = 10
 
     def update(self):
         '''Updates the position of the blast'''
@@ -30,5 +32,5 @@ class Blast:
     def draw(self):
         '''Draws the blast in the updated position'''
         _log.info("In Blast draw")
-        pyxel.circ(self.x, self.y, self.radius, BLAST_COLOR_IN)
-        pyxel.circb(self.x, self.y, self.radius, BLAST_COLOR_OUT)
+        pyxel.circ(self.x, self.y, self.radius, self.BLAST_COLOR_IN)
+        pyxel.circb(self.x, self.y, self.radius, self.BLAST_COLOR_OUT)
---
+++ project/test/test_blast.py
@@ -0,0 +1,41 @@
+import unittest
+from unittest.mock import patch
+from blast.model import Blast, BLAST_START_RADIUS, BLAST_END_RADIUS
+
+class TestBlast(unittest.TestCase):
+    def setUp(self):
+        '''Setting up for the test'''
+        self.x = 10
+        self.y = 20
+        self.blast = Blast(self.x, self.y)
+
+    def test_init(self):
+        '''Testing the __init__ method'''
+        self.assertEqual(self.blast.x, self.x)
+        self.assertEqual(self.blast.y, self.y)
+        self.assertEqual(self.blast.radius, BLAST_START_RADIUS)
+        self.assertTrue(self.blast.alive)
+        self.assertIn(self.blast, Blast.blast_list)
+
+    def test_update(self):
+        '''Testing the update method'''
+        self.blast.update()
+        self.assertEqual(self.blast.radius, BLAST_START_RADIUS + 1)
+
+        # Simulate reaching the end radius
+        self.blast.radius = BLAST_END_RADIUS
+        self.blast.update()
+        self.assertFalse(self.blast.alive)
+
+    @patch('blast.model.pyxel.circ')
+    @patch('blast.model.pyxel.circb')
+    def test_draw(self, mock_circ, mock_circb):
+        '''Testing the draw method'''
+        self.blast.draw()
+        mock_circ.assert_called_once_with(self.x, self.y, self.blast.radius,
+                                          Blast.BLAST_COLOR_IN)
+        mock_circb.assert_called_once_with(self.x, self.y, self.blast.radius,
+                                           Blast.BLAST_COLOR_OUT)
+
+if __name__ == '__main__':
+    unittest.main()
