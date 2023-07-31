d292f07cd688ad93ed58467a972b97fa9639a5fd
ettynan <ettynan@gmail.com>
2023-07-31 16:44:56 -0700

    Adding tests for game

--- project/test/test_game.py
+++ project/test/test_game.py
@@ -1,6 +1,6 @@
 import unittest
 import pyxel
-from game import App, SCENE_PLAY
+from game import App, SCENE_PLAY, SCENE_GAMEOVER, SCENE_TITLE
 
 def setup_module(module):
     pyxel.init(120, 160, title="All your base are belong to us!")
@@ -13,6 +13,15 @@ class TestApp(unittest.TestCase):
     def test_update_play_scene(self):
         self.app.update_play_scene()
         self.assertEqual(self.app.scene, SCENE_PLAY)
+
+    def test_update_title_scene(self):
+        self.app.update_title_scene()
+        self.assertEqual(self.app.scene, SCENE_TITLE)
+
+    def test_update_gameover_scene(self):
+        self.app.scene = SCENE_GAMEOVER
+        self.app.update()
+        self.assertEqual(self.app.scene, SCENE_GAMEOVER)
 
 if __name__ == "__main__":
     unittest.main()
