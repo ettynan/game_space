fb8f6a93f90281c785601a10bbf0270948e65a34
ettynan <ettynan@gmail.com>
2023-07-31 16:55:17 -0700

    Tests for gam are working now

--- project/test/test_game.py
+++ project/test/test_game.py
@@ -6,15 +6,17 @@ def setup_module(module):
     pyxel.init(120, 160, title="All your base are belong to us!")
 
 class TestApp(unittest.TestCase):
-    def setUp(self):
-        self.app = App()
-        self.app.scene = SCENE_PLAY
+    @classmethod
+    def setUpClass(cls):
+        cls.app = App()
 
     def test_update_play_scene(self):
+        self.app.scene = SCENE_PLAY
         self.app.update_play_scene()
         self.assertEqual(self.app.scene, SCENE_PLAY)
 
     def test_update_title_scene(self):
+        self.app.scene = SCENE_TITLE
         self.app.update_title_scene()
         self.assertEqual(self.app.scene, SCENE_TITLE)
 
